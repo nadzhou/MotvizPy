c902739a409e479c2356227f8ae69579b4959a3c
nadzhou <ahmaddnavid@gmail.com>
2020-03-11 13:18:38 +0500

    comments made

--- scripts/motviz/stats.py
+++ scripts/motviz/stats.py
@@ -3,6 +3,12 @@ import numpy as np
 from Bio import SeqIO
 
 def seq_extract(in_file): 
+    """Extrct the sequence from the fasta file
+    Args: 
+        input file [str]
+    REturns: 
+        Sequence [1d list]
+    """
     record = SeqIO.parse(in_file, "fasta")
     seqs = []
     for i in record: 
@@ -10,10 +16,16 @@ def seq_extract(in_file):
         
     return seqs
 
-def seq2np(seqs): 
-    return np.array(seq)
-
 def vert_seq(np_seq): 
+    """
+    Do a vertical manipulation on the array. 
+    Take out the sequence vertically
+    
+    Args: 
+        Numpy sequence [nd array]
+    Returns: 
+        Vertical sequence from each nd array [nd array]    
+    """
     def _per_column(array): 
         return array
 
@@ -21,7 +33,15 @@ def vert_seq(np_seq):
     
 seq = seq_extract("/home/nadzhou/Desktop/navid.fasta")
 
-
+# I was trying to do direct conversion 
+# but doesn't seem to happen 
 np_seq = np.array([np.array(y, dtype=object) for y in seq], dtype=object)
 
-print(np_seq.ndim)
+# ndim only gives me 1
+# but when I go inside, 
+# gives me the internal dim as well 
+
+# possibly numpy problem? 
+
+print(np_seq)
+print(np_seq[0].shape)
