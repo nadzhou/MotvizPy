bd39cf1717df2dc1fc4d78dc6e24cf22b88c1846
nadzhou <ahmaddnavid@gmail.com>
2020-03-12 14:00:14 +0500

    object oriented

--- scripts/motviz/stats.py
+++ scripts/motviz/stats.py
@@ -15,36 +15,58 @@ def seq_extract(in_file):
     for i in record: 
         seqs.append(str(i.seq))
     return seqs
+class Analysis: 
+      
 
-def conservation_score(np_seq): 
-    """
-    Do a vertical manipulation on the array. 
-    Take out the sequence vertically
-    
-    Args: 
-        Numpy sequence [nd array]
-    Returns: 
-        Vertical sequence from each nd array [nd array]    
-    """
+    def __init__(self, seq): 
+        """
+        Take in a 2d sequence and then do 
+        a whole bunch of calculations. 
+        
+        Args: 
+            Sequence list [list of lists]
+            
+        Returns: 
+            Conservation score 
+            Chi-squared table 
+            etc etc
+        """
+        def seq2np(seq): 
+            return np.asarray(seq, dtype=object)
+        
+        np_seq = seq2np(seq)
+        self.seq = seq
+        self.np_seq = np_seq
+        print(self.np_seq)
+            
+    def conservation_score(self): 
+        """
+        Calculate the Shannon Entropy vertically
+        for each position in the amino acid msa sequence.
+        
+        Args: 
+            Numpy sequence [nd array]
+        Returns: 
+            Vertical-orientation conservation score [nd array]    
+        """
 
-    def _shannon(array): 
-        array = str(array)
-        entropy = 0 
-        for x in array: 
-            char_count = np.char.count(array, sub=x)
-            print(char_count)
-            p_x = float(char_count/len(array)) 
-            if p_x > 0: 
-                entropy += - p_x*math.log(p_x, 2)
-        return entropy
-    return np.apply_along_axis(_shannon, 0, np_seq)
-    
+        def _shannon(array): 
+            array = str(array)
+            entropy = 0 
+            for x in array: 
+                char_count = np.char.count(array, sub=x)
+                p_x = float(char_count/len(array)) 
+                if p_x > 0: 
+                    entropy += - p_x*math.log(p_x, 2)
+            return entropy
+        return np.apply_along_axis(_shannon, 0, self.np_seq)
+        
 
-    
-    
+        
+        
 seq = seq_extract("/home/nadzhou/Desktop/aligned1.fasta")
+seq = [[x for x in y] for y in seq]
 
-seq = [list(x) for x in seq]
-np_seq = np.asarray(seq, dtype=object)
-v = conservation_score(np_seq)
+c = Analysis(seq)
 
+print(c.conservation_score())
