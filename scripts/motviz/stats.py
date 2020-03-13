95891072450151027045a6476e026a4e9890c4eb
nadzhou <ahmaddnavid@gmail.com>
2020-03-13 13:00:50 +0500

    data visualization

--- scripts/motviz/stats.py
+++ scripts/motviz/stats.py
@@ -2,6 +2,8 @@
 import numpy as np
 from Bio import SeqIO
 import math
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 def seq_extract(in_file): 
     """Extrct the sequence from the fasta file
@@ -16,12 +18,13 @@ def seq_extract(in_file):
         seqs.append(str(i.seq))
     return seqs
 class Analysis: 
-      
-
+    """Class to analyze the multiple sequence alignment. 
+    Implements the Shannon Entropy, chi-squared and 
+    per-column variation analysis algorithms. 
+    """        
     def __init__(self, seq): 
         """
-        Take in a 2d sequence and then do 
-        a whole bunch of calculations. 
+        Initialize class Analysis. 
         
         Args: 
             Sequence list [list of lists]
@@ -36,9 +39,9 @@ class Analysis:
         
         np_seq = seq2np(seq)
         self.seq = seq
-        self.np_seq = np_seq
-        print(self.np_seq)
-            
+        self.np_seq = np_seq   
+    
+
     def conservation_score(self): 
         """
         Calculate the Shannon Entropy vertically
@@ -55,11 +58,26 @@ class Analysis:
             entropy = 0 
             for x in array: 
                 char_count = np.char.count(array, sub=x)
-                p_x = float(char_count/len(array)) 
+
+                if x == "-": 
+                    p_x = float(char_count/len(array)*2) 
+                    
+                else: p_x = float(char_count/len(array))
                 if p_x > 0: 
-                    entropy += - p_x*math.log(p_x, 2)
+                    entropy += (- p_x*math.log(p_x, 2)/20)
             return entropy
-        return np.apply_along_axis(_shannon, 0, self.np_seq)
+        return np.apply_along_axis(_shannon, 0, self.np_seq)      
+
+    def normalize_data(self, ent_list): 
+        """Takes the entropy array and normalizes the data. 
+        
+        Args: 
+            Shannon Entropy array [numpy ndarray]
+        Returns: 
+            Normalized numpy array [numpy ndarray]
+        """
+        return (2.*(ent_list - \
+            np.min(ent_list))/np.ptp(ent_list)-1)       
         
 
         
@@ -69,4 +87,8 @@ seq = [[x for x in y] for y in seq]
 
 c = Analysis(seq)
 
-print(c.conservation_score())
+c_ent = c.conservation_score()
+a = c.normalize_data(c_ent)
+a_len = [x for x in range(len(a))]
+sns.lineplot(x=a_len, y=a)
+plt.show()
