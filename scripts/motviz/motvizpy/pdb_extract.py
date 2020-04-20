ec9ed055cb52baadf0762cf28c8061a7054e503c
nadzhou <ahmaddnavid@gmail.com>
2020-04-20 20:32:43 +0500

    polished pdb_extract

--- scripts/motviz/motvizpy/pdb_extract.py
+++ scripts/motviz/motvizpy/pdb_extract.py
@@ -4,6 +4,7 @@ import urllib.request
 from pathlib import Path
 
 
+import pandas as pd
 import seaborn as sns 
 import matplotlib.pyplot as plt
 
@@ -15,19 +16,23 @@ from Bio.PDB import *
 
 import re
 
+from pypdb import describe_pdb
+
 class MotifTable:
     """Tabulate the possible motifs"""
 
-    def retrieve_pdb_id(self, query_id):
+    def retrieve_pdb_id(self, query_ids_list):
         url = 'https://www.uniprot.org/uploadlists/'
 
+        query_ids = " ".join(query_ids_list)
 
         params = {
-            'from': 'ID',
-            'to': 'PDB_ID',
+            'from': 'P_REFSEQ_AC',
+            'to': 'ACC',
             'format': 'tab',
-            'query': query_id
+            'query': query_ids
             }
+
         data = urllib.parse.urlencode(params)
         data = data.encode('utf-8')
         req = urllib.request.Request(url, data)
@@ -38,16 +43,16 @@ class MotifTable:
         results = re.findall(r'[a-zA-Z0-9]{4}', results)
         no_duplicates = set(results)
         
-        for i in no_duplicates: 
-            print(i, end='\t')
+        return no_duplicates
 
 
-    def find_motif_pos(self, pos, seq):
+    def find_motif_pos(self, pos, seq, motif_stretch=4):
         """Find the sstretches of motifs with a given threshold
 
         Args:
             pos [list]: 1d list of the motif positions
             seq [list]: 1d list of the sequences
+            motif_stretch [int]: Position stretch to get sequence
 
         Returns:
             seq_residues [list]: Motifs traced from the seq from pos
@@ -58,8 +63,7 @@ class MotifTable:
         seq_residues = []
 
         for x,_ in enumerate(pos):
-            seq_residues.append(seq[x : x + 4])
-
+            seq_residues.append(seq[x : x + motif_stretch])
         return seq_residues
 
 
@@ -79,92 +83,126 @@ class MotifTable:
 
         return seq
 
-def pdb2fasta(file_path): 
-    """Retrieve the fasta file, description and
-     seq from irectory containing PDB files
 
-    Args: 
-        file_path [str]: Address for folder containing PDB files
+    def pdb2fasta(self, file_path): 
+        """Retrieve the fasta file, description and
+        seq from irectory containing PDB files
+
+        Args: 
+            file_path [str]: Address for folder containing PDB files
+        
+        """
+        file_path = Path(file_path)
+
+        for file in file_path.iterdir(): 
+            file_without_ext = str(file).split(".")
+            file_without_ext = file_without_ext[0]
+
+            record = SeqIO.parse(file, "pdb-seqres")
+
+            SeqIO.write(record, f"{file_without_ext}.fasta", "fasta")
+                    
+
+    def fasta2seq(self, file_path): 
+        """Extract all the fasta sequences from a directory 
+        
+        Args: 
+            file_path [str]: Address of the directory containing fasta files
+
+        Returns: 
+            seq [dict]: Sequences with their IDs as keys and sequences as values
+        
+        """
+        file_path = Path(file_path)
+        seqs = {}
+
+        for file in file_path.iterdir(): 
+            if "fasta" in file.name: 
+                record = SeqIO.parse(file, "fasta")
+                for x in record: 
+                    seqs[x.id] = x.seq
+        return seqs
+
+
+    def find_resis_in_pdb_seqs(self, seq_residues, seq_dict): 
+        found_in_seqs = []
+        for k, v in seq_dict.items(): 
+            for i in seq_residues: 
+                if i in v: 
+                    found_in_seqs.append(k)
+
+        return found_in_seqs
     
-    """
-    file_path = Path(file_path)
 
-    for file in file_path.iterdir(): 
-        file_without_ext = str(file).split(".")
-        file_without_ext = file_without_ext[0]
+    def describe_pdb_file(self, pdb_id): 
+        """Retrieve information from PyPDB on protein structure
 
-        record = SeqIO.parse(file, "pdb-seqres")
+        Args: 
+            pdb_id [list]: List of the PDB ids
 
-        SeqIO.write(record, f"{file_without_ext}.fasta", "fasta")
-                
+        returns: 
+            imp_data [dict]: Dictionary that contains, id, expmethod, resolution 
+        
+        """
 
-def fasta2seq(file_path): 
-    """Extract all the fasta sequences from a directory 
-    
-    Args: 
-        file_path [str]: Address of the directory containing fasta files
+        info_needed = ["structureId", "expMethod", "resolution","nr_residues", "nr_atoms", "status"]
+        imp_data = []
 
-    Returns: 
-        seq [dict]: Sequences with their IDs as keys and sequences as values
-    
-    """
-    file_path = Path(file_path)
-    seqs = {}
+        for id in pdb_id: 
+            results = describe_pdb(id)
+            temp = []
+            for k, v in results.items(): 
+                if k in info_needed: 
+                    temp.append(v)
+            imp_data.append(temp)
 
-    for file in file_path.iterdir(): 
-        if "fasta" in file.name: 
-            record = SeqIO.parse(file, "fasta")
-            for x in record: 
-                seqs[x.description] = x.seq
-    return seqs
+        return imp_data
+
+
+    def csv_writer(self, pdb_data, file_path): 
+        """Write the dictionary data as CSV file
+        
+        Args: 
+            pdb_data [list of lists]: List of lists of PDB info on id, exptmethod etc
+            file_path [str]: File path for writing the CSV file
+
+        """
+        df = pd.DataFrame.from_records(pdb_data, \
+            columns=["pdb_id", "expmethod", "resolution", "nr_residues", "nr_atoms", "status"])
+        df.index = df.index + 1
+        df.to_csv(file_path)
+
 
 
 
 def main(): 
+    motif_inst = MotifTable()
+
     in_file = "/home/nadzhou/Desktop/omega.aln"
     seq_dict = seq_extract(in_file, "clustal")
 
-    #seq_keys = list(seq_dict.keys())
-    #seq_keys = " ".join(seq_keys)
+    file_path = "/home/nadzhou/Desktop/pdbs"
+
+    seq_dict = motif_inst.fasta2seq(file_path)
+    seq_keys = list(seq_dict.keys())
+
+    orig_file = "/home/nadzhou/Desktop/pdbs/1xef.fasta"
+    seq2 = motif_inst.original_file_seq_extract(orig_file)
+
+    pos = [23, 42, 59, 14]
+    seq_residues = motif_inst.find_motif_pos(pos, seq2)
+
+    found_in_seqs = motif_inst.find_resis_in_pdb_seqs(seq_residues, seq_dict)    
+
+    pdb_data = motif_inst.describe_pdb_file(found_in_seqs)
+    print(pdb_data)
+    motif_inst.csv_writer(pdb_data, "/home/nadzhou/Desktop/pdb_results.csv")
 
 
-    # seq2 = pdb_inst.original_file_seq_extract(orig_file)
-
-
-    #seq_residues = pdb_inst.find_motif_pos(pos,  seq2)    
-
-    # fasta_file_path = "/home/nadzhou/Desktop/pdbs"
-    # c = fasta2seq(fasta_file_path)
-
-    # struct = list(c.keys()).split(" ")
-
-    # for i in struct: 
-    #     print(i)
-    # c = "".join(str(c).strip())
-
-    # print("Start \t Pattern \t Parent sequence location")
-
-    # for i in seq_residues: 
-    #     for m in re.finditer(str(i), c): 
-    #         print(f"{m.start()} \t {m.group()} \t {c[m.start() - 3 : m.end() + 5]}")
-
-    # plotter = {}
-
-    # for i in seq_residues: 
-    #     for k, v in seq_dict.items(): 
-    #         if i in v: 
-    #             plotter[k] = i
-
-    # print("ID - motif")
-    # for k, v in plotter.items(): 
-    #     print(f"{k, v}", end="\t\t")
-
-    # print("\nRefseq ID - PDB")
-    seq_keys = "P04637"
-    pdb_inst = MotifTable()
-    pdb_inst.retrieve_pdb_id(seq_keys)
 
 
 
 if __name__ == '__main__': 
     main()
+
+
