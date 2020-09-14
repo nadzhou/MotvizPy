e5fe47bb623e560b0ab8d53848cb8e25f5c17dbe
nadzhou <ahmaddnavid@gmail.com>
2020-09-14 12:31:35 +0500

    corrected main.py

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-par#!/usr/bin/env python3.7
+#!/usr/bin/env python3.7
 
 #!/usr/bin/env python
 
@@ -32,6 +32,7 @@ class Motviz:
 
         self.out_dir = Path(args.output_path)
         self.out_dir.mkdir(parents=True, exist_ok=True)
+        self.id_score = args.idscore
 
         self._motifs = None
     
@@ -64,7 +65,7 @@ class Motviz:
 
         for rec in needle_record[1:]: 
             reference_seq = rec[0]
-            seq_parser = IdenticalSequencesParser(reference_seq, rec[1])
+            seq_parser = IdenticalSequencesParser(reference_seq, rec[1], self.id_score)
 
             result = seq_parser.highly_identical_seqs()
             if result:
@@ -80,10 +81,10 @@ class Motviz:
         seqs = extract_seq(self.out_dir / "aligned_seq.fasta", "fasta")
         seq = [[x for x in y] for y in seqs]
 
-        c = Analysis(seq)
+        c =                                                                                                                                                                                                                                                                                                                                                             (seq)
 
-        c_ent = c.conservation_score()
-        self.norm_data = c.normalize_data(c_ent)
+        self.c_ent = c.conservation_score()
+        self.norm_data = c.normalize_data(self.c_ent)
 
         minima = c.find_local_minima(self.norm_data)
         pos_motif, pos = c.find_motif(self.norm_data, minima, threshold=-0.75)
@@ -91,9 +92,11 @@ class Motviz:
         print(f"Positions: {pos}")
         print(f"Possible motifs: {pos_motif}")
 
+        c.pymol_script_writer(self.out_dir / f"{self.pdb_id}_pymol.txt", pos)
+
 
     def plotter(self): 
-        norm_data_len = np.arange(1, len(self.norm_data) + 1)
+        norm_data_len = np.arange(1, len(self.c_ent) + 1)
 
         sns.lineplot(norm_data_len, self.norm_data)
         plt.title(f"Conservation score per amino acid position for PDB ID {self.pdb_id}")
@@ -104,16 +107,16 @@ class Motviz:
 
     def motifs(self): 
         if self._motifs == None: 
-            # self.struct_extract()
-            # self.pdb_seq_extract()
-            # print("PDB structure and sequence fetching: done.")
+            self.struct_extract()
+            self.pdb_seq_extract()
+            print("PDB structure and sequence fetching: done.")
 
-            # self.psi_blast()
-            # print("\nParsing PSI-BLAST results...")
-            # psi_results = self.parse_psi_xml()
-            # write_seq(psi_results, self.out_dir / "seqs.fasta")
+            self.psi_blast()
+            print("\nParsing PSI-BLAST results...")
+            psi_results = self.parse_psi_xml()
+            write_seq(psi_results, self.out_dir / "seqs.fasta")
 
-            # self.water()
+            self.water()
             self.seq_trimmer()
 
             try:  
@@ -129,7 +132,7 @@ class Motviz:
                 print("Could not find any meaningful target sequences.\nMotif finding failed: ", e)
 
 
-def main(): with
+def main(): 
     args = parse_arguments()
     mot = Motviz(args)
     mot.motifs()
